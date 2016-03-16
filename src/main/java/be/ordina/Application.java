21623bbf2bf07fed0773ebb9d2b84910eb1f6e1e
MarcoBrasci1986 <marco_brasci@hotmail.com>
2016-03-16 09:47:49 +0100

    renaming file

--- src/main/java/be/ordina/Application.java
+++ src/main/java/be/ordina/Application.java
@@ -2,7 +2,6 @@ package be.ordina;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -14,14 +13,12 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 @EnableAutoConfiguration
-@ComponentScan
+@ComponentScan // or just @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
-
 
 
 }
