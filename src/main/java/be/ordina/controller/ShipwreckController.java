5060538266698ec9208e64d5d61e15e2c4c7a1c5
MarcoBrasci1986 <marco_brasci@hotmail.com>
2016-03-18 13:49:07 +0100

    Adds  application.properties

---
+++ src/main/java/be/ordina/controller/ShipwreckController.java
@@ -0,0 +1,36 @@
+package be.ordina.controller;
+
+import be.ordina.domain.Shipwreck;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * Created by MaBa on 18/03/16.
+ */
+@RestController
+@RequestMapping("api/v1/")
+public class ShipwreckController {
+
+
+    @RequestMapping(value = "shipwrecks", method = RequestMethod.GET)
+    public List<Shipwreck> list() {
+        return ShipwreckStub.list();
+    }
+
+    @RequestMapping(value = "shipwrecks/{id}", method = RequestMethod.POST)
+    public Shipwreck create(@RequestBody Shipwreck shipwreck) {
+        return ShipwreckStub.create(shipwreck);
+    }
+
+    @RequestMapping(value = "shipwrecks", method = RequestMethod.PUT)
+    public Shipwreck update(@PathVariable Long id, @RequestBody Shipwreck shipwreck) {
+        return ShipwreckStub.update(id, shipwreck);
+    }
+
+    @RequestMapping(value = "shipwrecks/{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable Long id) {
+        ShipwreckStub.delete(id);
+    }
+
+}
